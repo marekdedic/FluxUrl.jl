400341bfe77de087bff844207b77af0466cf1723
Marek Dědič <developer@dedic.eu>
2017-09-25 13:47:16 +0200

    Fixed mean pooling

--- src/layers.jl
+++ src/layers.jl
@@ -8,4 +8,10 @@ ReLU(in::Int, out::Int) = Flux.Dense(in, out, Flux.relu; init = f32init);
 Linear(in::Int, out::Int) = Flux.Dense(in, out, identity; init = f32init);
 
 concatAggregation{T <: AbstractVector}(vecs::AbstractVector{T})::AbstractVector = vcat(vecs...);
-meanPooling{T <: AbstractVector}(vecs::AbstractVector{T})::AbstractVector = mean.(vecs...);
+meanPooling{T <: AbstractVector}(vecs::AbstractVector{T})::AbstractVector = begin
+	result = vecs[1];
+	for x in vecs[2:end]
+		result += x;
+	end
+	result /= length(vecs);
+end
